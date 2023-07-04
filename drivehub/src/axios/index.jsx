b2ec6c87b012297b41d02f3de78605524a9b5c3f
MikeSymoDev <mike04_norcal@yahoo.de>
2023-07-04 22:23:22 +0200

    First setup with all dependencies

---
+++ drivehub/src/axios/index.jsx
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://",
+// });
+
+// axiosInstance.defaults.baseURL =
+//   "https://";
+// axiosInstance.defaults.headers.post["Content-Type"] = "application/json";
+
+// export default axiosInstance;
