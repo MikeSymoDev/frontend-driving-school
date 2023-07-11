9c6e9b275fdaa3f58867a983adcf7e0e0a2ca3de
MikeSymoDev <mike04_norcal@yahoo.de>
2023-07-11 21:19:29 +0200

    links to logo and button, gradiant color, various small code adjustments and formatting

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,41 +5,46 @@ import { NavLink } from 'react-router-dom';
 import './header.scss';
 
 export default function Header() {
-
-
-
-
   return (
     <>
       <div className="header">
         <div className="leftheader">
           <div className="logo">
             {/* <BsCarFront size={30} /> */}
-              <img src={LogoWhite} alt='Drive Hub Logo' style={{ width: '7rem', height: 'auto' }} />
+            <NavLink to="/">
+              <img
+                src={LogoWhite}
+                alt="Drive Hub Logo"
+                style={{ width: '7rem', height: 'auto' }}
+              />
+            </NavLink>
           </div>
-
         </div>
         <div className="rightheader">
           <div className="navigation">
-
-            <NavLink className="navlink" to="/">HOME</NavLink>
-            <NavLink className="navlink" to="/alldrivers">DRIVERS</NavLink>
-            <NavLink className="navlink" to="driverprofile">MY PROFILE</NavLink>
-            <NavLink className="navlink" to="login">LOGIN</NavLink>
-            <NavLink className="navlink" to="signup">SIGN UP</NavLink>
+            <NavLink className="navlink" to="/">
+              HOME
+            </NavLink>
+            <NavLink className="navlink" to="/alldrivers">
+              DRIVERS
+            </NavLink>
+            <NavLink className="navlink" to="driverprofile">
+              MY PROFILE
+            </NavLink>
+            <NavLink className="navlink" to="login">
+              LOGIN
+            </NavLink>
+            <NavLink className="navlink" to="signup">
+              SIGN UP
+            </NavLink>
             <button className="logout">LOGOUT</button>
-
-
-
           </div>
         </div>
-      
       </div>
-        <div className='headerdivider'></div>
+      <div className="headerdivider"></div>
     </>
   );
 }
-
 
 // condtionioal linking to either student or driver provile:
 
@@ -49,7 +54,7 @@ export default function Header() {
 
 // const LinkComponent = ({ userType }) => {
 //   let linkDestination;
-  
+
 //   if (userType === 'student') {
 //     linkDestination = '/students';
 //   } else if (userType === 'driver') {
@@ -64,4 +69,3 @@ export default function Header() {
 //     </NavLink>
 //   );
 // };
-
