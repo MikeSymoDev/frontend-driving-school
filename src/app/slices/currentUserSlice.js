5423df61b1cdaec22618014161651a1b4a63afda
SvenZ <zbinden.sven@gmail.com>
2023-07-16 12:32:22 +0200

    login works, to do: get userprofile in response

--- src/app/slices/currentUserSlice.js
+++ src/app/slices/currentUserSlice.js
@@ -20,15 +20,27 @@ const loginUser = createAsyncThunk(
     "user/login", async (payload) => {
         const response = await axiosInstance.post("/token/", payload);
         const userData = {...response.data};
-        const id = userData.user.id;
         const token = userData.access;
-        const userName = userData.user.username;
-        const firstname = userData.user.first_name;
-        const lastname = userData.user.last_name;
-        const amountLikes = userData.user.amount_of_likes
-        const userState = {id, token, userName, firstname, lastname, amountLikes};
-        localStorage.setItem("currentUser", JSON.stringify(userState))
-        return userState;
+
+        const userState = {token}
+
+
+        const userInfoResponse = await axiosInstance.get('/user/instructors/2');
+        const userInfo = userInfoResponse.data;
+
+        console.log(userState)
+        console.log(userInfo)
+        // console.log(userData.user)
+        // console.log(id)
+        // const id = userData.user.id;
+        // const token = userData.access;
+        // const userName = userData.user.username;
+        // const firstname = userData.user.first_name;
+        // const lastname = userData.user.last_name;
+        // const amountLikes = userData.user.amount_of_likes
+        // const userState = {id, token, userName, firstname, lastname, amountLikes};
+        // localStorage.setItem("currentUser", JSON.stringify(userState))
+        // return userState;
     }
 )
 
