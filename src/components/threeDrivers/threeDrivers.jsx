a6b22bbe71a8047e929e39288fd52c23bd0350fe
MikeSymoDev <mike04_norcal@yahoo.de>
2023-07-17 10:35:43 +0200

    component finished with styling

--- src/components/threeDrivers/threeDrivers.jsx
+++ src/components/threeDrivers/threeDrivers.jsx
@@ -10,26 +10,50 @@ export default function ThreeDrivers() {
   const getDrivers = async () => {
     const response = await axiosInstance.get('/user/instructors/');
     setDrivers(response.data);
-    console.log(response.data);
+    // console.log(response.data);
   };
 
   useEffect(() => {
     getDrivers();
   }, []);
 
+  const getRandomDrivers = (arr, count) => {
+    const shuffled = arr.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+  };
+
+  const randomDrivers = getRandomDrivers(drivers, 3);
+
   return (
     <>
-      {drivers.map((driver) => (
-        <div key={driver.id}>
-          <p>Email: {driver.email}</p>
-          <p>Name: {driver.user.username}</p>
-          <img
-            src={driver.profile_image}
-            alt="Face"
-            style={{ width: '200px', height: 'auto' }}
-          />
+      <div className="threedrivers">
+        <div className="drivertitle">
+          <h2>SOME OF OUR DRIVERS</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
         </div>
-      ))}
+
+        <div className="drivers">
+          {randomDrivers.slice(0, 3).map((driver) => (
+            <div className="driverdetails" key={driver.id}>
+              <img
+                src={driver.profile_image}
+                alt="drivers"
+                style={{ width: '200px', height: '200px', borderRadius: '50%' }}
+              />
+              <h3>
+                {driver.user.first_name} {driver.user.last_name}
+              </h3>
+              <p>{driver.about}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Hero from '../components/hero/hero'
-import ThreeDrivers from '../components/threeDrivers/threeDrivers'
+import React from 'react';
+import './home.scss';
+import Hero from '../components/hero/hero';
+import ThreeDrivers from '../components/threeDrivers/threeDrivers';
 
 export default function Home() {
   return (
-    <div>
-      
-      <Hero />
-      <h1>Homepage Main Area</h1>
-      <ThreeDrivers />
+    <>
+      <div className="hero">
+        <Hero />
+      </div>
 
-    </div>
-  )
+      <div className="threedrivers">
+        <ThreeDrivers />
+      </div>
+    </>
+  );
 }
