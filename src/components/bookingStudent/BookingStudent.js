181fe2d09d2b32b7c0e2373dbef0128286e37b96
SvenZ <zbinden.sven@gmail.com>
2023-07-24 21:06:14 +0200

    styled booking page

--- src/components/bookingStudent/BookingStudent.js
+++ src/components/bookingStudent/BookingStudent.js
@@ -2,33 +2,29 @@ import React, { useEffect, useState } from "react";
 import "../bookingStudent/BookingStudent.scss";
 import "../../styles/_variables.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { bookAppointment, fetchAppointmentsByDate } from "../../app/slices/bookingSlice";
+import {bookAppointment,fetchAppointmentsByDate,} from "../../app/slices/bookingSlice";
 import { useParams } from "react-router-dom";
 
 const BookingComponent = () => {
-
   const bookingState = useSelector((store) => store.bookings)
 
   const currentDate = new Date().toISOString().split('T')[0];
 
   const [selectedDate, setSelectedDate] = useState(currentDate);
-  const [availableTimes, setAvailableTimes] = useState([]);
 
   const dispatch = useDispatch();
   const [date, setDate] = useState("");
   const [appointments, setAppointments] = useState([]);
-
-
-
-
   const { id } = useParams();
   const [instructorId, setInstructorId] = useState(null);
+
 
   useEffect(() => {
     const parsedInstructorId = parseInt(id, 10);
     setInstructorId(parsedInstructorId);
     fetchAppointmentsByDateHandler(selectedDate, parsedInstructorId);
   }, [dispatch, bookingState.booked, selectedDate, id]);
+
 
   const handleDateChange = (e) => {
     const selectedDate = e.target.value;
@@ -48,7 +44,14 @@ const BookingComponent = () => {
     }
   };
 
-  //Format Date and Time
+  const formatTime = (timeString) => {
+    const time = new Date(`2000-01-01T${timeString}`);
+    return time.toLocaleTimeString("de", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString("de", {
@@ -58,13 +61,6 @@ const BookingComponent = () => {
     });
   };
 
-  const formatTime = (timeString) => {
-    const time = new Date(`2000-01-01T${timeString}`);
-    return time.toLocaleTimeString("de", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  };
 
   const compareAppointmentsByStartTime = (a, b) => {
     const timeA = new Date(`2000-01-01T${a.start_time}`);
@@ -81,6 +77,15 @@ const BookingComponent = () => {
     return STATE_CHOICES[state] || "";
   };
 
+  const mapStateToClass = (state) => {
+    const STATE_CLASSES = {
+      O: "open",
+      B: "booked",
+      NA: "not-available",
+    };
+    return STATE_CLASSES[state] || "";
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
   };
@@ -92,9 +97,9 @@ const BookingComponent = () => {
 
   return (
     <div>
-      <h1 className="book">Book an Appointment</h1>
+      <h2 className="book">Book an Appointment</h2>
       <form className="form" onSubmit={handleFormSubmit}>
-        <label htmlFor="appointmentDate">Choose A Date: </label>
+        <label className="label" >Choose A Date: </label>
         <input
           className="datepicker"
           type="date"
@@ -107,31 +112,27 @@ const BookingComponent = () => {
       {appointments && appointments.length > 0 ? (
         <div className="time-cards-container">
           {appointments
-            .slice()
-            .sort(compareAppointmentsByStartTime)
+            .slice() // Create a copy of the array to avoid mutating the original
+            .sort(compareAppointmentsByStartTime) // Sort the copied array
             .map((appointment) => (
-              <div key={appointment.id} className="time-card">
-                      <h3>Start Slot: {formatTime(appointment.start_time)}</h3>
-                      <h6>Date: {formatDate(appointment.date)}</h6>
-                      <h6>State: {mapStateToLabel(appointment.state)}</h6>
-      
-                      <h6>
-                        Instructor:{" "}
-                        {appointment.instructor.first_name +
-                          " " +
-                          appointment.instructor.last_name}
-                      </h6>
-                      <h6>Email: {" " + appointment.instructor.email}</h6>
-                      <h6>Location:{" " + appointment.location}</h6>
-                      {appointment.state === "NA" && <p className="taken">BOOK</p>}
-                      {appointment.state === "B" && <p className="taken">BOOK</p>}
-                      {appointment.state === "O" && (
-                        <button onClick={() => handleBookAppointment(appointment.id)}>
-                          BOOK
-                        </button>
-                      )}
-            </div>
-          ))}
+              <div key={appointment.id} className={`time-card ${mapStateToClass(appointment.state)}`}>
+                <h3>Start Slot: {formatTime(appointment.start_time)}</h3>
+                <h6>Date: {formatDate(appointment.date)}</h6>
+                <h6>State: {mapStateToLabel(appointment.state)}</h6>
+
+                <h6>
+                  Instructor:{" "}
+                  {appointment.instructor.first_name +
+                    " " +
+                    appointment.instructor.last_name}
+                </h6>
+                <h6>Email: {" " + appointment.instructor.email}</h6>
+                <h6>Location:{" " + appointment.location}</h6>
+                {appointment.state === "NA" && (<button disabled onClick={()=> handleBookAppointment(appointment.id)}>BOOK</button>)}
+                {appointment.state === "B" && (<button disabled onClick={()=> handleBookAppointment(appointment.id)}>BOOK</button>)}
+                {appointment.state === "O" && (<button onClick={() => handleBookAppointment(appointment.id)}>BOOK</button>)}
+              </div>
+            ))}
       </div>
     ) : (
       <div className="No-Appoinments">
@@ -139,7 +140,7 @@ const BookingComponent = () => {
       </div>
     )}
   </div>
-);
+  );
 };
 
 export default BookingComponent;
